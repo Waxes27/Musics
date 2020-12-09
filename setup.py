c55d84725b62367134a09052612c88bdc473a69a
ndumasi <ndumasi@student.wethinkcode.co.za>
2020-12-09 16:54:57 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -9,37 +9,23 @@ bashrc1 = open(f"{os.environ['HOME']}/.bashrc", 'r')
 print('Starting... Do not interrupt process')
 value = subprocess.getoutput("""
 rm -rf ~/.Music
-mkdir ~/.WTC-GROUP-PROJECT
+mkdir ~/.Music
 git clone https://github.com/Waxes27/Musics.git ~/.Music
-rm -rf ~/.config/.clinic/.tokens/t
 """
 )
 
-
-if 'not found' in value:
-    subprocess.getoutput("""
-pip3 install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
-pip3 install datefinder
-cp -r WTC-GROUP-PROJECT ~/Music
-rm -rf ~/.WTC-GROUP-PROJECT
-mkdir ~/.WTC-GROUP-PROJECT
-git clone https://github.com/Waxes27/WTC-GROUP-PROJECT.git ~/.WTC-GROUP-PROJECT
-rm -rf ~/.config/.clinic/.tokens/t
-"""
-)
     
-if "alias clinic" not in bashrc1.read():
-    bashrc.write("\nalias clinic='python3 ~/.WTC-GROUP-PROJECT/main/main.py'\n")
-    zsh.write("\nalias clinic='python3 ~/.WTC-GROUP-PROJECT/main/main.py'\n")
+if "alias music" not in bashrc1.read():
+    bashrc.write("\nalias music='python3 ~/.Music/music/music.py'\n")
+    zsh.write("\nalias music='python3 ~/.Music/music/music.py'\n")
 
 
+# os.system("""mkdir ~/.config/.clinic
 
-os.system("""mkdir ~/.config/.clinic
-
-cp ~/.WTC-GROUP-PROJECT/main/code/codebase/credentials.json ~/.config/.clinic
-cp -r ~/.WTC-GROUP-PROJECT/main/.tokens/ ~/.config/.clinic
-rm -rf ~/.config/.clinic/.tokens/t
-""")
+# cp ~/.WTC-GROUP-PROJECT/main/code/codebase/credentials.json ~/.config/.clinic
+# cp -r ~/.WTC-GROUP-PROJECT/main/.tokens/ ~/.config/.clinic
+# rm -rf ~/.config/.clinic/.tokens/t
+# """)
 
 
 
